fdf379a2d16310563cc2d4efad4e1ae86142cb90
Sahil Jain <sahil@remind101.com>
2015-03-15 23:10:02 -0700

    fix pr comments

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ app.controller('navbarController', ['$scope', 'Auth', function($scope, Auth) {
 		$scope.isLoggedIn = function(){
 			return Auth.getToken().length > 0;
 		};
-        $scope.isLoggedOut = function(){
-            return !Auth.getToken().length >0;
-        }
 		$scope.logout = function(){
 			Auth.logout();
 		};
